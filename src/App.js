396f67cb083e53c33273f3f185d23b5499512c50
Vijay Shankar <91515683+CodeVijay53@users.noreply.github.com>
2022-12-08 11:34:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import data from "./data/data.json";
+
+export default function App() {
+  const [itemCount, setItemCount] = useState(0);
+  const manageItems = (e) => {
+    if (e.target.innerText === "Add to cart") {
+      setItemCount(itemCount + 1);
+      e.target.innerText = "Remove from cart";
+      e.target.classList.value = "btn btn-primary mt-auto";
+    } else if (e.target.innerText === "Remove from cart") {
+      setItemCount(itemCount - 1);
+      e.target.innerText = "Add to cart";
+      e.target.classList.value = "btn btn-success mt-auto";
+    }
+  };
+  return (
+    <>
+      <Navbar itemCount={itemCount} />
+      <Header />
+      <Cards data={data} manageItems={manageItems} />
+      <Footer />
+    </>
+  );
+}
